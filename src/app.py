bcda4f7f5c6878558b9c71c9be737a1bb11e1fbd
Agustin <jagustinbassi@gmail.com>
2020-06-10 17:50:59 -0300

    Add capacity to load and save data into a JSON file

--- src/app.py
+++ src/app.py
@@ -12,19 +12,21 @@
 
 #########[ Imports ]########################################################### 
 
+import os
+import json
+
 from flask import Flask, Response, abort, json, jsonify, request
 
 #########[ Settings & Data ]###################################################
 
+DB_FILE_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 
+    "../db/db.json"
+)
+# Flask App object
 app = Flask(__name__)
-
-# Put app configurations here, like endpoints, variables, and others.
-app_config = {
-    "PREFIX"    : "/",
-    "APP_PORT"  : 5000,
-    "key1"      : "value1",
-    "key2"      : "value2"
-}
+# Application config dict
+app_data = {}
 
 #########[ Utils ]#############################################################
 
@@ -38,32 +40,62 @@ def json_response(response, status_code):
         status=status_code
     )
 
+def load_app_data_from_db_file():
+    global app_data
+    # Read data from DB file
+    app_data = json.loads(open(DB_FILE_PATH).read())
+    # Print app data read
+    print("Data readed from DB file: " + str(app_data))
+
+def save_current_app_data_to_db_file():
+    # Save current data into DB file
+    with open(DB_FILE_PATH, 'w') as db_file:
+        # save app data in json pretty mode
+        json.dump(
+            app_data, 
+            db_file, 
+            ensure_ascii=False, 
+            indent=4
+            )
+    # Log the action to console
+    print("Updated DB file with new app data")
+
 #########[ Views (route resources) ]###########################################
 
-@app.route(app_config.get("PREFIX") + '', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_app_config():
     # create response for all devices
-    response = jsonify(app_config)
+    response = app_data
     # return the response with the status code
-    return response, 200
+    return json_response(response, 200)
 
-@app.route(app_config.get("PREFIX") + '', methods=['PUT'])
+@app.route('/', methods=['PUT'])
 def set_app_config():
-    global app_config
+    global app_data
 
     if not request.json:
         return json_response(
-            {'error' : 'Impossible to parse request body'}, 422
+            {'error' : 'Impossible to parse request body'}, 
+            422
             )
 
-    app_config.update(request.json)
-    response = app_config
+    app_data.update(request.json)
+    save_current_app_data_to_db_file()
+    response = app_data
 
     return json_response(response, 200)
 
 #########[ Module main code ]##################################################
 
+def init_app_data():
+    load_app_data_from_db_file()
+
 if __name__ == '__main__':
-    app.run()
+    init_app_data()
+    app.run(
+        host=app_data.get("APP_HOST"), 
+        port=app_data.get("APP_PORT"),
+        debug=app_data.get("APP_DEBUG"),
+        )
 
 #########[ Enf of file ]#######################################################
